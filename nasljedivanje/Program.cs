cec199157d11567cfca6828d2aa96dc339e54901
aleino0 <113008053+aleino0@users.noreply.github.com>
2022-11-09 09:31:28 +0100

    Add files via upload

---
+++ nasljedivanje/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasljedivanje
+{
+    class Program
+    {
+        class Cat
+        {
+            bool opasna;
+            public bool Opasna { get => opasna; set => opasna = value; }
+
+            public virtual void Opasnost()
+            {
+                this.opasna = false;
+            }
+            public override string ToString()
+            {
+                return "Maca - opasnost = " + this.opasna;
+            }
+        }
+        class Geopard : Cat
+        {
+            public override void Opasnost()
+            {
+                this.Opasna = true;
+            }
+            public override string ToString()
+            {
+                return "Geopard - opasnost: " + this.Opasna;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Geopard g = new Geopard();
+            g.Opasnost();
+            Console.WriteLine(g.ToString());
+
+            Cat maca = new Cat();
+            maca.Opasnost();
+            Console.WriteLine(maca.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
